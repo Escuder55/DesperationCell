118be00954b110e8589d2c9b641b7a01af119928
Sergio <Sergio.escuder5@gmail.com>
2020-04-03 20:34:17 +0200

    Selection Effect Done && Map Almost Done

---
+++ Desperation Cell/Assets/SelectObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour
+{
+    public GameObject outliner;
+
+    public void OnMouseOver()
+    {
+        Debug.Log("RATON ENCIMA");
+        outliner.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("RATON SALE");
+        outliner.SetActive(false);
+    }
+}
