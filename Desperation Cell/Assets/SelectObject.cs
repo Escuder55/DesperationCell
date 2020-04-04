796e797d26a14843e4424561970e1b7390ba7aa8
Sergio <Sergio.escuder5@gmail.com>
2020-04-04 22:22:57 +0200

    Few Changes

--- Desperation Cell/Assets/SelectObject.cs
+++ Desperation Cell/Assets/SelectObject.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SelectObject : MonoBehaviour
 {
     public GameObject outliner;
+    public Transform destinationPosition;
+
+    public Transform target;
+    public float speed;
 
     public void OnMouseOver()
     {
-        Debug.Log("RATON ENCIMA");
+        
         outliner.SetActive(true);
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveObject();
+        }
     }
 
     public void OnMouseExit()
     {
-        Debug.Log("RATON SALE");
+        
         outliner.SetActive(false);
+    }
+
+    void MoveObject()
+    {
+        this.transform.DOMove(target.position, speed);
     }
 }
