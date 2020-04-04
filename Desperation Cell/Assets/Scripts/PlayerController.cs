a2b77d6c0dddb51b7efd02d42450f13e042bcf17
Ignacio <ignacioperezvelasco@enti.cat>
2020-04-04 13:32:31 +0200

    PlayerMovementDone

---
+++ Desperation Cell/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //MOVE AGENT
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
