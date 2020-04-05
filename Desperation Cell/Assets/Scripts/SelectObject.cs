26750df706ed5eacd12c5f94db4ef8e3b7be73dd
Sergio <Sergio.escuder5@gmail.com>
2020-04-05 04:16:29 +0200

    Map Done

---
+++ Desperation Cell/Assets/Scripts/SelectObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SelectObject : MonoBehaviour
+{
+    public GameObject outliner;
+    public Transform target;
+    public float speed;
+    public bool movable = false;
+
+    public void OnMouseOver()
+    {
+        
+        outliner.SetActive(true);
+        if (Input.GetMouseButtonDown(0) && movable)
+        {
+            MoveObject();
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        
+        outliner.SetActive(false);
+    }
+
+    void MoveObject()
+    {
+        this.transform.DOMove(target.position, speed);
+    }
+}
